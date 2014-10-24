667122f35711b681a3c4d567d115ab14f903ef92
Marc <marc.bluemner@gmail.com>
2014-10-24 18:55:03 +0200

    created hobbies enum

--- app/models/hobbies.rb
+++ app/models/hobbies.rb
@@ -1,8 +1,12 @@
 class Hobbies
   include Mongoid::Document
+  include Mongoid::Enum 
+
   field :id, type: Integer
   field :hobby_name, type: String
   field :user, type: Member
+
+  enum :hobbies, [:reading, :running, :travel, :cafe], :multiple => true
 
   embedded_in :member
 end
