3f8712f186c12711ed0a055277c36a175396032b
Marc <marc.bluemner@gmail.com>
2014-10-24 16:48:25 +0200

    member_info

---
+++ app/models/member_info.rb
@@ -0,0 +1,10 @@
+class MemberInfo
+  include Mongoid::Document
+  field :id, type: Integer
+  field :age, type: Integer
+  field :gender, type: String
+  field :smoker, type: Mongoid::Boolean
+  field :drinker, type: Mongoid::Boolean
+  field :relashionship_status, type: String
+  field :children, type: Integer
+end
